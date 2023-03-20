ff7ecafad004f754cf3046f77c8f556afad5c4ba
Sathish9686 <princesathish9686@gmail.com>
2023-03-20 13:19:02 +0000

    Destination Search

---
+++ src/components/DestinationSearch/index.js
@@ -0,0 +1,51 @@
+// Write your code here
+
+import {Component} from 'react'
+import DestinationItem from '../DestinationItem'
+import './index.css'
+
+class DestinationSearch extends Component {
+  state = {searchInput: ''}
+
+  onclickSearch = event => {
+    this.setState({searchInput: event.target.value})
+  }
+
+  render() {
+    const {searchInput} = this.state
+    console.log(searchInput)
+    const {destinationsList} = this.props
+    const searchResult = destinationsList.filter(each =>
+      each.name.toLowerCase().includes(searchInput.toLowerCase()),
+    )
+
+    return (
+      <div className="app-container">
+        <div className="destinations-search-container">
+          <h1 className="heading">Destination Search</h1>
+          <div className="search-input-container">
+            <input
+              type="search"
+              className="search-input"
+              placeholder="Search"
+              value={searchInput}
+              onChange={this.onclickSearch}
+            />
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/destinations-search-icon-img.png"
+              alt="search icon"
+              className="search-icon"
+            />
+          </div>
+          <ul className="destinations-list">
+            {searchResult.map(eachItem => (
+              <DestinationItem destinationsList={eachItem} key={eachItem.id} />
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default DestinationSearch
